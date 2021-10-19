affda824d0878cf68b1fd27a988d74ef5f5e2587
MD EALIUS AZAM SHA <ealiusazamsha@gmail.com>
2021-10-20 03:01:31 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator1
+{
+    public partial class Form1 : Form
+    {
+        Double resultValue = 0;
+        string operationPerformed = "";
+        bool isoprationPerformed = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_click(object sender, EventArgs e)
+        {
+            if ((textBox_result.Text == "0") || (isoprationPerformed))
+                textBox_result.Clear();
+            
+            isoprationPerformed = false;
+            Button button = (Button)sender;
+            if (button.Text.Contains("."))
+            {
+                if(!textBox_result.Text.Contains("."))
+                    textBox_result.Text = textBox_result.Text + button.Text;
+
+            }else
+
+            textBox_result.Text = textBox_result.Text + button.Text;
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (resultValue !=0)
+            {
+
+                button11.PerformClick();
+                operationPerformed = button.Text;
+                labelCurrentOperation.Text = resultValue + "" + operationPerformed;
+                isoprationPerformed = true;
+
+
+            }
+            else
+            {
+
+
+                operationPerformed = button.Text;
+                resultValue = Double.Parse(textBox_result.Text);
+                labelCurrentOperation.Text = resultValue + "" + operationPerformed;
+                isoprationPerformed = true;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox_result.Text = "0";
+            resultValue = 0;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            switch (operationPerformed)
+            {
+                case "+":
+                    textBox_result.Text = (resultValue + Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_result.Text = (resultValue - Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_result.Text = (resultValue * Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_result.Text = (resultValue / Double.Parse(textBox_result.Text)).ToString();
+                    break;
+                default:
+                    break;
+
+
+            }
+            resultValue = Double.Parse(textBox_result.Text);
+            labelCurrentOperation.Text = "";
+        }
+    }
+}
